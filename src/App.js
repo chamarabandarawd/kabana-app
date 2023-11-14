e6cdab1c22db5e1b9931b9555a7b4e87fe1d0eab
chamarabandarawd <chamarabandarawd@gmail.com>
2023-11-14 21:39:56 +0530

    Navibar Done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import Layout from './components/Layout';
+
+
+
 
 function App() {
   return (
-    <div>
-      <header className='p-4 flex items-center justify-between'>
-        <a href='' className='flex items-center gap-1'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205l3 1m1.5.5l-1.5-.5M6.75 7.364V3h-3v18m3-13.636l10.5-3.819" />
-          </svg>
-          <span className='font-bold text-xl'> Kabana</span>
-        </a>
-
-        <div className='flex border border-gray-300 rounded-full py-1 px-2 gap-1'>
-          <div>Anywhere</div>
-          <div>Any week</div>
-          <div>Add guests</div>
-          <button>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-            </svg>
-
-          </button>
-        </div>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path='/login' element={<LoginPage />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const MainPage = () => {
+  return (
+   <h1>Main page</h1>
+  )
+}
+
+export default MainPage
